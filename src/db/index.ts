2229ec5a0d6ffc3d515a7b26489d1a999ebd664c
Aleksandr <a.teslin90@gmail.com>
2023-10-09 13:53:43 +0300

    feat: add db with hardcoded data

---
+++ src/db/index.ts
@@ -0,0 +1,79 @@
+export const SEARCH_AUTOCOMPLETE_DB = [
+  {
+    id: '1',
+    title: 'React js',
+    url: 'https://en.wikipedia.org/wiki/React_(software)',
+    description:
+      'React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library[3][4] for building user interfaces based on components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies',
+  },
+  {
+    id: '2',
+    title: 'React js tutorial',
+    url: 'https://www.simplilearn.com/tutorials/reactjs-tutorial',
+    description:
+      'React is the most popular front-end JavaScript library in the field of web development. React is a JavaScript library created for building fast and interactive user interfaces for web and mobile applications. It is an open-source, component-based, front-end library responsible only for the application’s view layer. React gained popularity due to some of its salient features  - can be used for the development of both web and mobile apps, unidirectional data flow, reusable components, creation of dynamic applications, and many more. In our React tutorial, we cover all such features, explain what exactly React is and the benefits of using it, it’s components, state, and more. So start learning with our React tutorial, and broaden your knowledge and hone your skills now.',
+  },
+  {
+    id: '3',
+    title: 'React JSX',
+    url: 'https://legacy.reactjs.org/docs/introducing-jsx.html',
+    description:
+      'This funny tag syntax is neither a string nor HTML. It is called JSX, and it is a syntax extension to JavaScript. We recommend using it with React to describe what the UI should look like. JSX may remind you of a template language, but it comes with the full power of JavaScript. JSX produces React “elements”. We will explore rendering them to the DOM in the next section. Below, you can find the basics of JSX necessary to get you started.',
+  },
+  {
+    id: '4',
+    title: 'React js documentation',
+    url: 'https://react.dev/learn',
+    description:
+      'Welcome to the React documentation! This page will give you an introduction to the 80% of React concepts that you will use on a daily basis.',
+  },
+  {
+    id: '5',
+    title: 'React js interview questions',
+    url: 'https://www.simplilearn.com/tutorials/reactjs-tutorial/reactjs-interview-questions',
+    description:
+      "React is quite the buzzword in the industry these days. As of now, React is the most popular front-end technology that more and more companies are using, and if you are preparing for a job interview, this is ReactJS interview questions tutorial is just the right one for you. Here's a comprehensive list of all the common ReactJS interview questions from basic to advanced levels that are frequently asked in interviews. ",
+  },
+  {
+    id: '6',
+    title: 'React json schema form',
+    url: 'https://rjsf-team.github.io/react-jsonschema-form/docs/',
+    description:
+      'react-jsonschema-form is meant to automatically generate a React form based on a JSON Schema. If you want to generate a form for any data, sight unseen, simply given a JSON schema, react-jsonschema-form may be for you. If you have a priori knowledge of your data and want a toolkit for generating forms for it, you might look elsewhere. react-jsonschema-form also comes with tools such as uiSchema and other form props to customize the look and feel of the form beyond the default themes.',
+  },
+  {
+    id: '7',
+    title: 'React jwt',
+    url: 'https://www.permify.co/post/jwt-authentication-in-react/',
+    description:
+      'In this article, we’re gonna build a demo app which demonstrates how to manage authentication in React.js using JWT. Why these two? simply React is the widely used frontend framework (personally my favorite), and JSON Web Token, is the most used authentication protocol on the web. Because it’s such a common way to manage authentication in client side applications with JWT. I’ll try to highlight best practices rather than how to implement it.',
+  },
+  {
+    id: '8',
+    title: 'React jss',
+    url: 'https://cssinjs.org/react-jss/?v=v10.10.0',
+    description:
+      'React-JSS integrates JSS with React using the new Hooks API. JSS and the default preset are already built in. Try it out in the playground. The HOC based API is deprecated as of v10 and may be removed in a future version. You can still perform a lazy migration as described here. HOC specific docs are available here.',
+  },
+  {
+    id: '9',
+    title: 'React jsx for loop',
+    url: 'https://www.telerik.com/blogs/beginners-guide-loops-in-react-jsx',
+    description:
+      'Wondering how to create loops in React? Learn about JSX and how to use methods like the map function to loop inside React JSX and render a list of items.',
+  },
+  {
+    id: '10',
+    title: 'React js hooks',
+    url: 'https://legacy.reactjs.org/docs/hooks-intro.html',
+    description:
+      'This new function useState is the first “Hook” we’ll learn about, but this example is just a teaser. Don’t worry if it doesn’t make sense yet!',
+  },
+  {
+    id: '11',
+    title: 'React Query',
+    url: 'https://tanstack.com/query/v3/',
+    description:
+      'Toss out that granular state management, manual refetching and endless bowls of async-spaghetti code. TanStack Query gives you declarative, always-up-to-date auto-managed queries and mutations that directly improve both your developer and user experiences.',
+  },
+];
